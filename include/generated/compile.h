4df5eb7b982024e0623673f547b64db45a83ea19
cj_000 <cj_000@live.com>
2012-03-08 19:41:12 -0500

    Kexec as module kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#7 SMP PREEMPT Sun Nov 20 00:18:16 EST 2011"
+#define LINUX_COMPILE_TIME "00:18:16"
+#define LINUX_COMPILE_BY "CJ"
+#define LINUX_COMPILE_HOST "cj"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.1.2"
